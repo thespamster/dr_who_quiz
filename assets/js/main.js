de647ac4d521cbcf7f3eddbbaba58f13f6a31f29
Gavin Brown <iamslowtrain@gmail.com>
2023-09-24 15:50:11 +0100

    add 42 quiz questions and 126 answers as an array to the javascript.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,497 @@
-// Get the modal
-var modal = document.getElementById("rulesModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("buttonRules");
+// array containing quiz questions and answers //
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let drWhoQuiz = [
+  {
+      "question": "Sydney Newman, creator of Dr. Who said that didn't want what 'appearing in the show'?",
+      "answers": [
+          "Bug Eyed Monsters",
+          "Swivel Eyed Loons",
+          "Googly Eyed Horrors",
+      ],
+      "correct": 0
+  },
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+  {
+      "question": "Ron Grainer wrote the music for Dr. Who but who was the genius that turned it into the electronic masterpiece that we know and love?",
+      "answers": [
+          "Delia Derbyshire",
+          "Delia Smith",
+          "Verity Lambert",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "What is the name of Earth's twin planet first seen in the 1st Doctor's adventure 'The Tenth Planet'?",
+      "answers": [
+          "Mondas",
+          "Uranus",
+          "Mongo",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "Which actor played the first Doctor as the show began on 23rd November 1963?",
+      "answers": [
+          "William Hartnell",
+          "Edmund Warwick",
+          "David Bradley",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "Complete the name of this first Doctor adventure, Galaxy... ?",
+      "answers": [
+          "4",
+          "Zero",
+          "9",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "Peter Purves played two characters on the show. One was called Stephen. What was the other called?",
+      "answers": [
+          "Morton Dill",
+          "Sabalom Glitz",
+          "Terry Nation",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "The Doctor's race, the Timelords, first appear in the second Doctor story called?",
+      "answers": [
+          "The War Games",
+          "The Seeds of Death",
+          "The Mind Robber",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "The second Doctor loved to play what musical instrument?",
+      "answers": [
+          "The recorder",
+          "The penny whistle",
+          "The saxophone",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "The second Doctor met the robot Yeti's twice during his adventures. How was the roar of the Yeti created?",
+      "answers": [
+          "Slowing down the sound of a flushing toilet",
+          "Slowing down the sound of a bear roaring",
+          "Recording an actual Yeti",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "The Doctor's famous sonic screwdriver first appeared in which second Doctor adventure?",
+      "answers": [
+          "Fury from the Deep",
+          "The Underwater Menace",
+          "The Moonbase",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "The second Doctor also appeared in a third Doctor story, called?",
+      "answers": [
+          "The Three Doctors",
+          "The Two Doctors",
+          "The Four Doctors",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "Which actor portrayed the second Doctor, first appearing in the 1966 adventure 'The Power of the Daleks?",
+      "answers": [
+          "Patrick Troughton",
+          "Ramon Salamander",
+          "Frazer Hines",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "Which third Doctor story was the first Dr. Who adventure to be broadcast in colour?",
+      "answers": [
+          "Spearhead from Space",
+          "Inferno",
+          "The Ambassadors of Death",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "Jo Grant played by Katy Manning left the show at the end of which third Doctor story?",
+      "answers": [
+          "The Green Death",
+          "Invasion of the Dinosaurs",
+          "The Claws of Axos",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "The third Doctor met he daleks three times, what was the name of the third dalek story?",
+      "answers": [
+          "Death to the Daleks",
+          "Day of the Daleks",
+          "Planet of the Daleks",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "The third Doctor drove a famous yellow car called?",
+      "answers": [
+          "Bessie",
+          "Nessie",
+          "Jessie",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "The Autons apppeared in the first story of regenerated Who in 2005 but they appeared first in the third Doctor adventure called?",
+      "answers": [
+          "Spearhead from Space",
+          "The Claws of Axos",
+          "Terror of the Autons",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "'Five rounds rapid' is a famous line spoken by the Brigadier in which third Doctor story?",
+      "answers": [
+          "The Daemons",
+          "The Mutants",
+          "The Time Warrior",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "In the fourth Doctor story 'The Horror of Fang Rock' the arch enemies of the Sontarans make an appearance. What are they called?",
+      "answers": [
+          "Rutans",
+          "Sea Devils",
+          "Ogrons",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "Which actor portrayed the Fourth Doctor?",
+      "answers": [
+          "Tom Baker",
+          "Michael Bentine",
+          "Bernard Cribbins",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "The fourth Doctor's companion Romana was played by two actors, one was Mary Tamm, the other was?",
+      "answers": [
+          "Lalla Ward",
+          "Barbara Windsor",
+          "Juliet Landau",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "In the fourth Doctor's adventure 'Warriors Gate' what are the race of lion like people called?",
+      "answers": [
+          "Tharils",
+          "Leonids",
+          "Krynoids",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "What's the name of the multimillionaire botanist in the fourth Doctor's adventure 'The Seeds of Doom'?",
+      "answers": [
+          "Harrison Chase",
+          "Harrison Ford",
+          "Ben Aaronovitch",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "During the fourth Doctor's run a very famous author was the script writer/editor for the show. What was his name?",
+      "answers": [
+          "Douglas Adams",
+          "Zaphod Beeblebrox",
+          "Neil Gaiman",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "In the fifth Doctor's story 'Earthshock' something happened to a companion that had never happend before. What was it?",
+      "answers": [
+          "A companion died",
+          "The first Doctor/companion kiss",
+          "The first Doctor/alien kiss",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "Which actor portrayed the fifth Doctor?",
+      "answers": [
+          "Peter Davison",
+          "Richard Griffiths",
+          "Eric Saward",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "For the 20th anniversary of the show in  1983 the fifth Doctor had an adventure called?",
+      "answers": [
+          "The five Doctors",
+          "The three Doctors",
+          "The two Doctors",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "In 'Timeflight' the fifth Doctor flew on a famous aircraft. Was it...?",
+      "answers": [
+          "Concorde",
+          "The Wright brother's plane 'Kittyhawk'",
+          "An Avro Lancaster World War 2 bomber",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "In the fifth Doctor adventure 'Kinda' the people who are not Kinda are known as what?",
+      "answers": [
+          "The not we",
+          "The others",
+          "The outsiders",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "This fifth Doctor adventure saw the return of the Mara and an appearance by a young Martin Clunes. What was his character called?",
+      "answers": [
+          "Lon",
+          "Don",
+          "Won",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "Which actor portrayed the sixth Doctor?",
+      "answers": [
+          "Colin Baker",
+          "Michael Grade",
+          "Jon Nathan Turner",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "What were the sixth Doctor's last words spoken on tv?",
+      "answers": [
+          "'Carrot juice'",
+          "'Grape juice'",
+          "Apple juice'",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "The actor who played the sixth Doctor has a dubious distinction that no other actor that has played the role can claim. What is it?",
+      "answers": [
+          "He's the only actor to be fired from the role",
+          "He's the only actor to quit the role during filming",
+          "He's the only actor to be prosecuted during their tenure",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "Parts 9-12 of the sixth Doctor's story 'Trial of a Timelord' eventually became known as?",
+      "answers": [
+          "Terror of the Vervoids",
+          "30 seconds to Doomsday",
+          "Revenge of The Master",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "Doctor Who has always been famous for its guest stars. Which famous comedian guest starred in the adventure 'Revelation of the Daleks?",
+      "answers": [
+          "Alexei Sayle",
+          "Ben Elton",
+          "Rik Mayall",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "The actor who played sixth Doctor appeared as a character during the fifth Doctor's adventure 'Arc of Infinity. What was his character called?",
+      "answers": [
+          "Maxil",
+          "Lord Cranleigh",
+          "Stien",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "In the seventh Doctor's first story he battles another renegade Timelord played by Kate O'Mara. What was her character called?",
+      "answers": [
+          "The Rani",
+          "The Master",
+          "The Time Meddler",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "The Doctor has enjoyed enjoyed playing various instruments. What did the seventh Doctor play?",
+      "answers": [
+          "The spoons",
+          "The harmonica",
+          "The accordian",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "Name the famous entertainer who guest starred in the seventh Doctor's adventure 'The Curse of Fenric?",
+      "answers": [
+          "Nicholas Parsons",
+          "Les Dennis",
+          "Ken Dodd",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "Which actor portrayed the seventh Doctor until the end of the classic era in 1989?",
+      "answers": [
+          "Sylvester McCoy",
+          "Leonard Nimoy",
+          "Paul McGann",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "In the seventh Doctor's adventure 'Remembrance of the Daleks' a very unusual dalek was seen for the first time. What was it called?",
+      "answers": [
+          "Special weapons Dalek",
+          "Reconnaissance Dalek",
+          "Emporer Dalek",
+      ],
+      "correct": 0
+  },
+
+  {
+      "question": "Which of the Doctor's companions starred in the final show of the classic era 'Survival'?",
+      "answers": [
+          "'Ace' played by Sophie Aldred",
+          "'Mel' played by Bonnie Langford",
+          "'Ray' played by Sara Griffiths",
+      ],
+      "correct": 0
+  },
+  
+]
+
+// timer function for 30 second countdown //
+
+function timer() {
+  if (timeLeft == -1) {
+    document.getElementById("countdown-timer").innerHTML = "0" + timeLeft;
+    clearInterval(timerId);
+    endGame();
+    return
+  } else if (timeLeft >= 10) {
+    document.getElementById("countdown-timer").innerHTML = timeLeft;
+  } else {
+    document.getElementById("countdown-timer").innerHTML = "0" + timeLeft;
+  }
+  rndNum3 = Math.floor(Math.random() * 3);
+  rndNum42 = Math.floor(Math.random() * 42);
+  console.log(timeLeft, rndNum3, rndNum42);
+  timeLeft --;
+  
+
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+function game() {
+  var initialScore = 0;
+  var initialHighScore = 0;
+  console.log(drWhoQuiz[0].question, drWhoQuiz[0].answers[0]);
+  console.log(startedGame);
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  var rulesModal = document.getElementById("rulesModal");
+  var rulesBtn = document.getElementById("buttonRules");
+  var rulesSpan = document.getElementsByClassName("close")[0];
+  var buttonStart = document.getElementById("buttonStart");
+
+  rulesBtn.onclick = function() {
+    console.log(startedGame);
+    if (startedGame) {
+      return;
+    } else {
+    rulesModal.style.display = "block";
+  }
+
+  rulesSpan.onclick = function() {
+    rulesModal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == rulesModal) {
+      rulesModal.style.display = "none";
+    }
   }
 }
+
+  buttonStart.onclick = function() {
+    if (startedGame) {
+      return;
+      } else {
+      startedGame = true;
+      timerId = setInterval(timer, 1000);
+    }
+  }
+}
+
+function endGame() {
+  alert("Game Over");
+}
+
+var startedGame = false;
+let timeLeft = 30;
+game();
+
+
+
